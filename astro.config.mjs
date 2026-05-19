0f8e20bb5e3fded3ea3a1337398816c53f7c8e70
yasminastro <yasminastro@outlook.com>
2026-05-19 16:32:15 +0200

    setup github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://yasminastro.github.io",
+	base: "/logicastro",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
